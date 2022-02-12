b756ecb950df535beefecd343657909f31ce52f9
alxb421 <alxb421@gmail.com>
2022-02-12 20:16:36 +0200

    fixed jumpstart

--- jumpstart-basic.sh
+++ jumpstart-basic.sh
@@ -1,71 +1,70 @@
 !#/bin/bash
 
-
-#untested btw dont complain about gambas or debtap
-
-
-
-#get root access
+# Simple Check to make sure it is executed as root
 if [[ ! "$EUID" == "0" ]]; then
   echo "Run as root pls"
   exit 1
 fi
-#get other stuff
+
+# Install requirements
 pacman -S --needed git make cmake linux-headers libreoffice vi vim python
-#get yay
+
+# Install yay (to download from the aur repo)
 cd /opt
 sudo git clone https://aur.archlinux.org/yay-git.git
-sudo chown -R pear:pear ./yay-git
-cd yay-git
+sudo chown -R $USER:$USER ./yay-git && cd yay-git
 makepkg -si
-#get gui pacman
+
+# Install pacman GUI
 yay -S pamac-aur
-#get gambas
-pacman -S gambas
+
+# Install gambas3 (required to run pearOS apps)
+pacman -S gambas3
+
 #get pear apps
-mkdir ~/pearApps
-cd ~/pearApps
-mkdir control-centre
-cd control-centre
-wget https://github.com/alxb421/Control-Centre/releases/download/11.0.1/xyz.pearos.control-centre_11.1.0_all.deb
-pacman -S fakeroot binutils
-yay -S debtap
-debtap -U
-package=$(xyz.pearos.control-centre_11.1.0_all.deb)
-debtap $package
-pacmanpkg=$(echo *.pkg.tar.xz)
-pacmanpkg=$(echo *.pkg.tar.zst)
-pacman -U $pacmanpkg
-cd ~/pearApps
-git clone https://github.com/alxb421/pext-installer.git
-cd pext-installer
-mv pext-installer /usr/share/
-cd pext-installer/system-update /tmp
-python /usr/share/pext-installer/pext-installer
-cd ~/pearApps
-git clone https://github.com/alxb421/piri-backend.git
-cd piri-backend
-chmod +x install.sh
-./install.sh
-cd ~/pearApps
-mkdir sys-overview
-cd sys-overview
-wget https://github.com/alxb421/system-overview/releases/download/11.0.1/xyz.pearos.system-overview_11.1.0_all.deb
-debtap -U
-pkg=$(xyz.pearos.system-overview_11.1.0_all.deb)
-debtap $pkg
-pacmanpkg=$(echo *.pkg.tar.xz)
-pacmanpkg=$(echo *.pkg.tar.zst)
-pacman -U $pacmanpkg
-cd~/pearApps
-mkdir updatemgr
-cd updatemgr
-wget https://github.com/alxb421/update-mgr/releases/download/11.1/xyz.pearos.update-mgr_11.1_all.deb
-debtap -U
-pckg=$(https://github.com/alxb421/update-mgr/releases/download/11.1/xyz.pearos.update-mgr_11.1_all.deb)
-debtap $pckg
-pacmanpkg=$(echo *.pkg.tar.xz)
-pacmanpkg=$(echo *.pkg.tar.zst)
-pacman -U $pacmanpkg
+#mkdir ~/pearApps
+#cd ~/pearApps
+#mkdir control-centre
+#cd control-centre
+#wget https://github.com/alxb421/Control-Centre/releases/download/11.0.1/xyz.pearos.control-centre_11.1.0_all.deb
+#pacman -S fakeroot binutils
+#yay -S debtap
+#debtap -U
+#package=$(xyz.pearos.control-centre_11.1.0_all.deb)
+#debtap $package
+#pacmanpkg=$(echo *.pkg.tar.xz)
+#pacmanpkg=$(echo *.pkg.tar.zst)
+#pacman -U $pacmanpkg
+#cd ~/pearApps
+#git clone https://github.com/alxb421/pext-installer.git
+#cd pext-installer
+#mv pext-installer /usr/share/
+#cd pext-installer/system-update /tmp
+#python /usr/share/pext-installer/pext-installer
+#cd ~/pearApps
+#git clone https://github.com/alxb421/piri-backend.git
+#cd piri-backend
+#chmod +x install.sh
+#./install.sh
+#cd ~/pearApps
+#mkdir sys-overview
+#cd sys-overview
+#wget https://github.com/alxb421/system-overview/releases/download/11.0.1/xyz.pearos.system-overview_11.1.0_all.deb
+#debtap -U
+#pkg=$(xyz.pearos.system-overview_11.1.0_all.deb)
+#debtap $pkg
+#pacmanpkg=$(echo *.pkg.tar.xz)
+#pacmanpkg=$(echo *.pkg.tar.zst)
+#pacman -U $pacmanpkg
+#cd~/pearApps
+#mkdir updatemgr
+#cd updatemgr
+#wget https://github.com/alxb421/update-mgr/releases/download/11.1/xyz.pearos.update-mgr_11.1_all.deb
+#debtap -U
+#pckg=$(https://github.com/alxb421/update-mgr/releases/download/11.1/xyz.pearos.update-mgr_11.1_all.deb)
+#debtap $pckg
+#pacmanpkg=$(echo *.pkg.tar.xz)
+#pacmanpkg=$(echo *.pkg.tar.zst)
+#pacman -U $pacmanpkg
 cd ~
 #end
