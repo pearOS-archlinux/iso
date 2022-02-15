f03946bf455553cd5ca25152b61e9a649d201161
alxb421 <alxb421@gmail.com>
2022-02-15 05:38:32 +0200

    fixed big part of the code

--- make.sh
+++ make.sh
@@ -5,7 +5,6 @@ set -e
 ESC=$(printf '\033') RESET="${ESC}[0m" BLACK="${ESC}[30m" RED="${ESC}[31m"
 GREEN="${ESC}[32m" YELLOW="${ESC}[33m" BLUE="${ESC}[34m" MAGENTA="${ESC}[35m"
 CYAN="${ESC}[36m" WHITE="${ESC}[37m" DEFAULT="${ESC}[39m"
-
 ### Color Functions ##
 greenprint() { printf "${GREEN}%s${RESET}\n" "$1"; }
 blueprint() { printf "${BLUE}%s${RESET}\n" "$1"; }
@@ -17,11 +16,8 @@ fn_bye() { echo "Program terminated: [user_quit]"; exit 0; }
 fn_fail() { echo "Program terminated: [wrong_option]"; exit 1; }
 fn_noroot() { echo "Program terminated: [user_notroot]"; exit 1; }
 
-if [ $(id -u) != 0 ]; then
-     fn_noroot
- fi
+OUT=$(mktemp -d)
 
-# create out directory
 create_out() {
 	echo "Creating out directory..."
 	mkdir -p out
@@ -37,11 +33,31 @@ clean_out() {
 
 # the ISO building script
 actual_build() {
+	# start a simple timer
 	start=`date +%s`
-	./mkarchiso -v -w ./out -o . pear
+
+	# creates randomly named dir. This tool uses it as cache so it needs to be removed many time by hand.
+	mkdir -p "$OUT"
+
+	# copy files from packages dir to where arch needs them
+	cp -r packages/pacman.out pear/pacman.conf
+	cp -r pear/pacman.conf pear/airootfs/etc/.
+	cp packages/packages.x86_64 pear/airootfs/etc/packages.x86_64
+	cp /etc/pacman.d/mirrorlist pear/airootfs/etc/pacman.d/.
+
+	# makes iso
+	sudo ./mkarchiso -v -w $OUT -o . pear
+
+	# ends the timer, calculates time ended minus time started, printing result in seconds
 	end=`date +%s`
 	runtime=$((end-start))
 	echo "The command was completed in $runtime s"
+
+	# removes working direcory
+	sudo rm -rf "$OUT"
+
+	# make current user the owner of the iso file
+	sudo chown $USER:$USER *.iso
 }
 
 mainmenu() {
