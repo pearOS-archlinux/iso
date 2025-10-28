48e1027fb37a8dbd0526e75cb5ca17cc403c8050
Alexandru Balan <alxb421@gmail.com>
2025-10-28 05:03:35 +0200

    update

---
+++ pear/airootfs/root/customize_airootfs.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#This script runds inside airootfs chroot during build
+# it's executed automatically by mkarchiso
+
+set -e -u
+
+echo "==========================="
+echo "Customizing pearOS Live env"
+echo "==========================="
+
+echo "Setting OS release"
+echo "pearOS" > /etc/arch-release && \
+	echo "OS release updated" ||\
+	echo "Failed to set OS release"
+
+if command -v plymouth-set-default-theme &> /dev/null; then
+	echo "Setting plymouth theme"
+	plymouth-set-default-theme -R pear-plymouth && \
+		echo "Success!" || \
+		echo "Failed to set plymouth theme"
+else
+	echo "Plymouth command theme not found"
+fi
+
+echo "==================="
+echo "Script run complete"
+echo "==================="
