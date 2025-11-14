020f8dae281a85f526fb460ee711ae3feeb3e637
Alexandru Balan <alxb421@gmail.com>
2025-11-15 01:20:16 +0200

    try fix

--- pear/airootfs/usr/share/pearOS-installer/post-install/app/resources/hello/script.js
+++
@@ -1,8 +0,0 @@
-const hello = document.querySelector(".hello__div");
-setInterval(hello__function, 20000);
-function hello__function() {
-  hello.style.display = "none";
-  setTimeout(function () {
-    hello.style.display = "flex";
-  }, 10);
-}
