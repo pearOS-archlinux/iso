c88c73327ee0496a90eefadba7bb92d2643b9a5d
Alexandru Balan <alxb421@gmail.com>
2026-03-11 15:32:02 +0200

    26.3

--- services.sh
+++ services.sh
@@ -2,22 +2,33 @@
 set -e
 cd pear/airootfs/etc/systemd/system/
 
-# enable sddm
+# 1. Display Manager & Graphical Target
 ln -sv /usr/lib/systemd/system/graphical.target default.target
 ln -sv /usr/lib/systemd/system/sddm.service display-manager.service
 
-# enable ew network manager
+# 2. Network Manager (NM)
+mkdir -p multi-user.target.wants
 ln -sv /usr/lib/systemd/system/NetworkManager.service multi-user.target.wants/NetworkManager.service
 ln -sv /usr/lib/systemd/system/NetworkManager-wait-online.service network-online.target.wants/NetworkManager-wait-online.service
 ln -sv /usr/lib/systemd/system/NetworkManager-dispatcher.service dbus-org.freedesktop.nm-dispatcher.service
 
-# enable printer
-mkdir printer.target.wants
-ln -sv /usr/lib/systemd/system/cups.service printer.target.wants/cups.service
+# 3. Printing (CUPS)
+ln -sv /usr/lib/systemd/system/cups.service multi-user.target.wants/cups.service
 ln -sv /usr/lib/systemd/system/cups.socket sockets.target.wants/cups.socket
 ln -sv /usr/lib/systemd/system/cups.path multi-user.target.wants/cups.path
 
-# enable bt
+# 4. Bluetooth
 ln -sv /usr/lib/systemd/system/bluetooth.service dbus-org.bluez.service
-mkdir bluetooth.target.wants
+mkdir -p bluetooth.target.wants
 ln -sv /usr/lib/systemd/system/bluetooth.service bluetooth.target.wants/bluetooth.service
+ln -sv /usr/lib/systemd/system/bluetooth.service multi-user.target.wants/bluetooth.service
+
+# 5. Virtualization Support
+ln -sv /usr/lib/systemd/system/vmtoolsd.service multi-user.target.wants/vmtoolsd.service
+# VirtualBox
+ln -sv /usr/lib/systemd/system/vboxservice.service multi-user.target.wants/vboxservice.service
+ln -sv /usr/lib/systemd/system/qemu-guest-agent.service multi-user.target.wants/qemu-guest-agent.service
+
+# 6. Power & System Management
+ln -sv /usr/lib/systemd/system/power-profiles-daemon.service multi-user.target.wants/power-profiles-daemon.service
+ln -sv /usr/lib/systemd/system/fprintd.service multi-user.target.wants/fprintd.service
